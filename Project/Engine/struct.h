2a929637d5328a3bc2b8b92d769dcd4c6a314a25
moonseok-choi <moonseoktech@gmail.com>
2021-11-14 12:59:17 +0900

    struct 추

--- Project/Engine/struct.h
+++ Project/Engine/struct.h
@@ -73,3 +73,19 @@ struct Particle
 	int is_active;
 };
 
+
+struct PatrcleShadredData
+{
+	int activable_count;
+	int current_active_count;
+	Vec3 range;
+};
+
+struct GlobalData
+{
+	Vec2 resolution;
+	Vec2 noise_texture_resolution;
+	float delta_time;
+	float accumulated_time;
+	Vec2 padding;
+};
